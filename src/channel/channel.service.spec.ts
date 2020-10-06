d795e5bd1ba1264f77e733f706d24bce3a728981
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-10-06 23:05:37 +0200

    fix eslint issues

--- src/channel/channel.service.spec.ts
+++ src/channel/channel.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ChannelService } from './channel.service';
 
 describe('ChannelService', () => {
-  let service: ChannelService;
+    let service: ChannelService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ChannelService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [ChannelService],
+        }).compile();
 
-    service = module.get<ChannelService>(ChannelService);
-  });
+        service = module.get<ChannelService>(ChannelService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
