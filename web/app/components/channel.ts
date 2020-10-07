3410c280dacb6f98bcef934c40a6776e4d6b2dfa
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-10-07 18:49:26 +0200

    refactor to js project

--- web/app/components/channel.ts
+++
@@ -1,5 +0,0 @@
-import Component from '@glimmer/component';
-
-interface ChannelArgs {}
-
-export default class Channel extends Component<ChannelArgs> {}
--- web/types/session.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Session {
-  isAuthenticated: boolean,
-  data: Object,
-  attemptedTransition: any,
-  session: any,
-  store: any
-}
