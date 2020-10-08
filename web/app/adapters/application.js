72843aaf0792c22dbdddbe8581b8a3a84fd34c20
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-10-08 22:56:46 +0200

    add models

--- web/app/adapters/application.js
+++ web/app/adapters/application.js
@@ -1,4 +1,20 @@
 import RESTAdapter from '@ember-data/adapter/rest';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationAdapter extends RESTAdapter {
+  @service session;
+
+  namespace = "api";
+
+  // eslint-disable-next-line ember/require-computed-property-dependencies
+  header = computed('session.data.authenticated.access_token', function() {
+      let headers = {};
+      if (this.session.isAuthenticated) {
+          headers[
+              "Authorization"
+          ] = `Bearer ${this.session.data.authenticated.user.xa}`;
+      }
+      return headers;
+  })
 }
