7160f434aec3883cc5a42c900e6eabf80fbc7758
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-10-22 21:20:42 +0200

    setup client uploader

--- web/app/controllers/channels/info.js
+++ web/app/controllers/channels/info.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { task } from "ember-concurrency";
-import { set } from '@ember/object';
+import { set, action } from '@ember/object';
 
 export default class ChannelsInfoController extends Controller {
     @service session;
@@ -11,7 +11,37 @@ export default class ChannelsInfoController extends Controller {
         set(this, "account", yield this.currentUser.peek())
     })).on("init") getChannels;
 
-    episodes = null
-
     account = null
+
+    @task(function* (audio) {
+        console.log(yield audio);
+    }).maxConcurrency(3)
+        .enqueue() uploadToServer
+
+    @action
+    uploadAudio(audio) {
+        try {
+            set(this, "load", true);
+            set(this, "audio", audio);
+            set(this, "load", false);
+        } catch (error) {
+            set(this, "load", false);
+            console.error(error);
+        }
+    }
+
+    @action
+    save() {
+        try {
+            set(this, "loader", true);
+            if(this.audio && this.title) {
+                this.uploadToServer.perform(this.audio);
+            } else {
+                set(this, "loader", false);
+            }
+        } catch (error) {
+            set(this, "loader", false);
+            console.error(error)
+        }
+    }
 }
