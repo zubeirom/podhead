67629ba18cfa23409d051cc7386ce8c46fe28c2c
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-10-24 03:36:58 +0200

    Add file module for operations regarding files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { ChannelModule } from './channel/channel.module';
 import { EpisodeModule } from './episode/episode.module';
 import { AccountModule } from './account/account.module';
 import { FeedModule } from './feed/feed.module';
+import { FileModule } from './file/file.module';
 
 dotenv.config();
 
 @Module({
-    imports: [ChannelModule, EpisodeModule, AccountModule, FeedModule],
+    imports: [ChannelModule, EpisodeModule, AccountModule, FeedModule, FileModule],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/file/interfaces/file-service.interface.ts
@@ -0,0 +1,4 @@
+export interface IFileService {
+    upload()
+    stream()
+}
